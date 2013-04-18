e6f48971680458c68d325034a1b2a18871186a6e
jryder <jryder@L11716038605.wctc.msft>
2013-04-17 19:58:52 -0500

    First version, very basic architecture

---
+++ src/abstraction/OutputStrategy.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package abstraction;
+
+/**
+ *
+ * @author jryder
+ */
+public interface OutputStrategy {
+    public abstract void outputFile(String filename);
+}
---
+++ src/fileservice/FileService.java
@@ -0,0 +1,22 @@
+
+
+
+
+package fileservice;
+
+/**
+ *
+ * @author jryder
+ */
+public class FileService {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+        
+        
+    }
+}
